dddd5f8812dd2168fd3aeede202ee6c3319265ab
Gabriel Gardner <gabriel.k.gardner@gmail.com>
2025-07-15 18:17:21 -0500

    fix: underline color

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 
 const router = createRouter({
-  linkExactActiveClass: 'font-bold relative after:content-[""] after:absolute after:top-[24px] after:left-0 after:w-[21px] after:h-[2px] after:bg-yellow-400',
+  linkExactActiveClass: 'font-bold relative after:content-[""] after:absolute after:top-[24px] after:left-0 after:w-[21px] after:h-[2px] after:bg-kta-yellow',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
