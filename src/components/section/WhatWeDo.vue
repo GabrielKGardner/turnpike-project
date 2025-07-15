bedaf40b67c81ca0d50aa2f611fd1b9bccad5e88
Gabriel Gardner <gabriel.k.gardner@gmail.com>
2025-07-15 16:27:54 -0500

    feat: what-we-do section

---
+++ src/components/section/WhatWeDo.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Ellipse from '@/assets/images/ellipse.svg'
+import Card from '@components/ui/Card.vue'
+import Computer from '@/assets/icons/computer-icon.svg'
+import Mobile from '@/assets/icons/mobile-phone-icon.svg'
+import Rocket from '@/assets/icons/rocket-icon.svg'
+import Website from '@/assets/icons/website-icon.svg'
+</script>
+
+<template>
+  <div class="relative">
+    <img :src="Ellipse" alt="" class="absolute -bottom-20 right-0 -z-30" />
+    <div class="absolute -z-20 top-0 w-9/10 h-full bg-kta-light-blue rounded-tr-[400px]"></div>
+    <div class="flex flex-col gap-23 px-12 py-23">
+      <h2 class="text-5xl text-kta-purple text-center w-96 mx-auto">
+        <span class="font-bold">What We Do <br /></span>For Your
+        <span class="highlight">Business</span>
+      </h2>
+      <div class="flex gap-12">
+        <Card
+          title="WEBSITE DESIGN"
+          text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          color="pink"
+          :src="Website"
+        />
+        <Card
+          title="WEB DEVELOPMENT"
+          text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          color="light-yellow"
+          :src="Computer"
+        />
+        <Card
+          title="MOBILE APP DEVELOPMENT"
+          text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          color="light-blue"
+          :src="Mobile"
+        />
+        <Card
+          title="MARKETING"
+          text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          color="light-pink"
+          :src="Rocket"
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/ui/Card.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  title: String,
+  text: String,
+  color: 'pink' | 'light-yellow' | 'light-blue' | 'light-pink',
+  src: String,
+})
+const colorMap = {
+  pink: 'bg-kta-pink',
+  'light-yellow': 'bg-kta-light-yellow',
+  'light-blue': 'bg-kta-light-blue',
+  'light-pink': 'bg-kta-light-pink',
+}
+</script>
+
+<template>
+  <div class="bg-white drop-shadow-md rounded-lg p-5">
+    <img :src="src" alt="" :class="[colorMap[color], 'text-kta-purple size-12 rounded-lg mb-3']" />
+    <h3 class="text-kta-purple font-bold mb-1.5">{{ title }}</h3>
+    <p class="text-kta-light-purple text-sm">{{ text }}</p>
+  </div>
+</template>
