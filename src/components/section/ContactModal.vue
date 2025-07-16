358ca0fc6b3d733355b98aa7b1a9fff6b1756270
Gabriel Gardner <gabriel.k.gardner@gmail.com>
2025-07-16 09:07:09 -0500

    fix: typescript

--- src/components/section/ContactModal.vue
+++ src/components/section/ContactModal.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
 import Button from '@components/ui/Button.vue'
 import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle } from '@headlessui/vue'
--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 defineProps({
   title: String,
   text: String,
